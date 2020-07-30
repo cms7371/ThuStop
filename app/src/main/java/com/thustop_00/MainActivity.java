a633bac25219a8bc2280616bcaa4b1ef19876b93
cms7371 <cms7371@gmail.com>
2020-07-30 21:55:39 +0900

    MainActivity 누락 다시 커밋

--- app/src/main/java/com/thustop_00/MainActivity.java
+++ app/src/main/java/com/thustop_00/MainActivity.java
@@ -6,6 +6,25 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import com.thustop_00.databinding.ActivityMainBinding;
+import com.thustop_00.intro.IntroBaseFragment;
+
+
+/*
+ * Copyright (c) 2019 by ThusTop INC. all rights reserved
+ * - Project name : ThuStop
+ * - First written by Minseok Chae and Suwon Lee students of SungKyunKwan Univ, College of
+ *   Information and Communication Engineering(ICE) and developers of ThusTop INC
+ * - Created on 2020.07.28 / Last revision on 0000.00.00
+ * - Version Beta 00
+ * - Description : Android application for TheStop beta service.
+ * - Update history(Please enter when other developer modifies this project.)
+ *   - 20.07.28. Created as version Beta 00
+ */
+
 
 public class MainActivity extends AppCompatActivity {
 
