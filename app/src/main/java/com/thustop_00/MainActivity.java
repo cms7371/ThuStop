21e49fe9179a67dfacfbeeedc56d70cf3a1a16a7
cms7371 <cms7371@gmail.com>
2020-07-30 16:34:07 +0900

    MainActivity 프로젝트 주석 추가

--- app/src/main/java/com/thustop_00/MainActivity.java
+++ app/src/main/java/com/thustop_00/MainActivity.java
@@ -3,8 +3,25 @@ package com.thustop_00;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
+
+/*
+ * Copyright (c) 2019 by ThusTop INC. all rights reserved
+ * - Project name : ThuStop
+ * - First written by Minseok Chae and Suwon Lee students of SungKyunKwan Univ, College of
+ *   Information and Communication Engineering(ICE) and developers of ThusTop INC
+ * - Created on 2020.07.28 / Last revision on 0000.00.00
+ * - Version Beta 00
+ * - Description : Android application for TheStop beta service.
+ * - Update history(Please enter when other developer modifies this project.)
+ *   - 20.07.28. Created as version Beta 00
+ */
 public class MainActivity extends AppCompatActivity {
+
+    /* Handler for d*/
+    Handler H = new Handler(Looper.getMainLooper());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
