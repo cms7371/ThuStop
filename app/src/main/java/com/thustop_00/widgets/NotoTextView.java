695182158cc861636293f22bdb6706f26786432a
cms7371 <cms7371@gmail.com>
2020-07-30 15:35:01 +0900

    초기 작업 : assets 폴더와 필요한 NotoSans 폰트 추가 / widgets 폴더와 필요한 attrs.xml 생성 / NotoButton, NotoTextView 정의 / gradle에 ViewPager2와 CircleIndicator3 추가

---
+++ app/src/main/java/com/thustop_00/widgets/NotoTextView.java
@@ -0,0 +1,50 @@
+package com.thustop_00.widgets;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatTextView;
+
+import com.thustop_00.R;
+
+/*
+ * This class is for customizing TextView.
+ * Through the function 'setType', change the font of the button into NotoSansKR
+ */
+public class NotoTextView extends AppCompatTextView {
+    /* Below 3 methods are essential declarations with new function 'setType'*/
+    public NotoTextView(Context context) {
+        super(context);
+        setType(context, null);
+    }
+
+    public NotoTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setType(context, attrs);
+    }
+
+    public NotoTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setType(context, attrs);
+    }
+
+    /*
+     * Main method of this class.
+     * From attribute(res/values/attrs) NotoStyle's notoTextStyle, set font depend on integer.
+     */
+    private void setType(Context context, AttributeSet attrs) {
+        TypedArray attr = context.obtainStyledAttributes(attrs, R.styleable.NotoStyle);
+        switch (attr.getInt(R.styleable.NotoStyle_notoTextStyle, 1)) {
+            case 1: //When option is 'regular'
+                this.setTypeface(Typeface.createFromAsset(context.getAssets(), "NotoSansKR-Regular-Hestia.otf"));
+            case 2: //When option is 'medium'
+                this.setTypeface(Typeface.createFromAsset(context.getAssets(), "NotoSansKR-Medium-Hestia.otf"));
+            case 3: //When option is 'bold'
+                this.setTypeface(Typeface.createFromAsset(context.getAssets(), "NotoSansKR-Bold-Hestia.otf"));
+        }
+        attr.recycle();
+
+    }
+}
